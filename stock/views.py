87df57817c3e323cf3c098b1968545ef49800b9d
yuanchingchen <yuanching.chen.appledev@gmail.com>
2021-04-26 14:38:48 -0500

    [UPDATE] 1. Add stock info in home page  2. Modify stock tracking list UI

--- stock/views.py
+++ stock/views.py
@@ -6,6 +6,7 @@ import requests
 from stock.models import Stock
 from django.contrib import messages
 from .utilities import process_stock_raw_data
+from datetime import datetime
 
 KEY = "pk_9274979738c74528ad6d76dc1c59296a"
 base_url = "https://cloud.iexapis.com"
@@ -14,21 +15,32 @@ base_url = "https://cloud.iexapis.com"
 def home(request):
     stock_symbol = "aapl"
     search_result = {}
-
     if request.method == "POST":
         stock_symbol = request.POST["stock_quote_input"]
 
+    news_url = base_url + "/stable/stock/" + stock_symbol + "/news/last?token=" + KEY
+    raw_res = requests.get(news_url)
+    try:
+        search_result = json.loads(raw_res.content)
+
+        for news in search_result:
+            mil_sec = news["datetime"]
+            date_string = datetime.fromtimestamp(mil_sec // 1000)
+            news["datetime"] = date_string
+    except Exception as e:
+        res = "StockNewsRequestError"
+
     url = base_url + "/stable/stock/" + stock_symbol + "/quote?token=" + KEY
     raw_res = requests.get(url)
+    stock_detail = {}
 
     try:
         res = json.loads(raw_res.content)
-        search_result = process_stock_raw_data(res)
-
+        stock_detail = process_stock_raw_data(res)
     except Exception as e:
-        res = "StockQuoteRequestError"
+        stock_detail = "StockQuoteRequestError"
 
-    return render(request, 'home.html', {"search_result": search_result})
+    return render(request, 'home.html', {"search_result": search_result, "stock_detail": stock_detail})
 
 
 def stock_list(request):
@@ -42,6 +54,7 @@ def add_stock(request):
         url = base_url + "/stable/stock/" + stock_symbol + "/quote?token=" + KEY
         raw_res = requests.get(url)
         response_content = {}
+        all_tracking_stocks = {}
 
         try:
             res = json.loads(raw_res.content)
@@ -58,10 +71,12 @@ def add_stock(request):
                 ytd_change=response_content["ytdChange"],
             )
             stock_data.save()
+            messages.success(request, stock_symbol.upper() + "is added to your watch list successfully.")
         except Exception as e:
-            res = "StockQuoteRequestError"
+            all_tracking_stocks = "StockQuoteRequestError"
+            messages.warning(request, "Please enter a valid stock symbol.")
 
-        all_tracking_stocks = Stock.objects.all() #.order_by('stock_id')
+        all_tracking_stocks = Stock.objects.all().order_by('stock_id')
 
     return render(request, 'stock_list.html', {"stock_symbol_list": all_tracking_stocks})
 
@@ -69,7 +84,7 @@ def add_stock(request):
 def delete_stock(request, stock_id):
     target_stock = Stock.objects.get(pk=stock_id)
     target_stock.delete()
-    messages.warning(request, "Stock has been deleted successfully.")
+    messages.success(request, "Stock has been deleted successfully.")
     return redirect('stock_list')
 
 
